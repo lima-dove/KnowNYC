9db5494cb23a7020e396ccdc226eb34ec735139e
Svetlana Shinkar <svetanek@gmail.com>
2019-06-28 18:05:14 -0400

    feat: add markers by zip

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import MapGL, {Marker} from 'react-map-gl'
+import axios from 'axios'
 
 const token =
   'pk.eyJ1IjoibnNjaGVmZXIiLCJhIjoiY2p2Mml0azl1MjVtejQ0bzBmajZhOHViZCJ9.iPyB8tGgsYgboP_fKLQGnw'
@@ -8,6 +9,7 @@ export default class HomePage extends Component {
   constructor() {
     super()
     this.state = {
+      complaints: [],
       viewport: {
         latitude: 40.705,
         longitude: -74.009,
@@ -17,6 +19,17 @@ export default class HomePage extends Component {
       }
     }
   }
+  async componentDidMount() {
+    const {data} = await axios.get(
+      'https://data.cityofnewyork.us/resource/fhrw-4uyv.json?incident_zip=10004'
+    )
+    this.setState({
+      complaints: data
+      // address: data[0].incident_address,
+      // long: data[0].location.coordinates[0],
+      // lat: data[0].location.coordinates[1]
+    })
+  }
 
   handleClick = () => {
     const {history} = this.props
@@ -25,7 +38,12 @@ export default class HomePage extends Component {
   }
 
   render() {
-    const {viewport} = this.state
+    const {complaints, viewport} = this.state
+    console.log('COMPLAINTS====', complaints[0])
+    const locationComplaints = complaints.filter(
+      complaint => complaint.location
+    )
+
     return (
       <div>
         <MapGL
@@ -37,17 +55,24 @@ export default class HomePage extends Component {
           preventStyleDiffing={false}
           mapboxApiAccessToken={token}
         >
-          <Marker
-            latitude={40.705}
-            longitude={-74.009}
-            offsetLeft={-20}
-            offsetTop={-10}
-          >
-            <img
-              src="http://i.imgur.com/WbMOfMl.png"
-              onClick={this.handleClick}
-            />
-          </Marker>
+          {locationComplaints
+            ? locationComplaints.map(complaint => {
+                return (
+                  <Marker
+                    key={complaint.unique_key}
+                    latitude={complaint.location.coordinates[1]}
+                    longitude={complaint.location.coordinates[0]}
+                    offsetLeft={-20}
+                    offsetTop={-10}
+                  >
+                    <img
+                      src="http://i.imgur.com/WbMOfMl.png"
+                      onClick={this.handleClick}
+                    />
+                  </Marker>
+                )
+              })
+            : null}
         </MapGL>
       </div>
     )
