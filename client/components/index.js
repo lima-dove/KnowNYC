eb34cfb3bd46b3472147b614a00db708cb4be71f
Noah Schefer <nbschefer@gmail.com>
2019-06-28 16:12:50 -0400

    fix: change import statement in UserHome spec

--- client/components/index.js
+++ client/components/index.js
@@ -3,8 +3,8 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {Login, Signup} from './auth-form'
+export {Login, Signup} from './AuthForm'
 export {default as Users} from './Firebase'
 export {default as HomePage} from './HomePage'
-export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
+export {default as Navbar} from './Navbar'
+export {default as UserHome} from './UserHome'
