813476813c5c3a3ed025f816b8f7b0b8b2b8154b
Daniel Wasserman <daniel.h.wasserman@gmail.com>
2019-06-29 19:17:14 -0400

    FEAT: add styling to info page

--- client/components/InfoPage.js
+++ client/components/InfoPage.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import axios from 'axios'
 import Typography from '@material-ui/core/Typography'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import LineGraph from './LineGraph'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Paper from '@material-ui/core/Paper'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import {fade, withStyles} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import PropTypes from 'prop-types'
+import SearchIcon from '@material-ui/icons/Search'
+import InputBase from '@material-ui/core/InputBase'
 
-class ExampleComplaints extends React.Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  },
+  inputRoot: {
+    color: 'inherit'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200
+      }
+    }
+  }
+})
+
+class InfoPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -58,25 +112,83 @@ class ExampleComplaints extends React.Component {
     )
   }
   render() {
-    const data = this.state.lineGraphData
+    const {classes} = this.props
+
     return (
       <div>
-        <Typography variant="h1" align="center" color="inherit">
-          Address: {this.state.address}
+        <div className={classes.root}>
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Data for given address
+              </Typography>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                  }}
+                  inputProps={{'aria-label': 'Search'}}
+                />
+              </div>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <br />
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <Card>
+                <CardContent>
+                  <LineGraph />
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Card>
+                <CardContent>
+                  <LineGraph />
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
           <br />
-          Long/Lat: [{this.state.long}, {this.state.lat}]
-        </Typography>
-        {this.state.complaints.map(complaint => {
-          return (
-            <ListItem key={complaint.unique_key}>
-              <ListItemText>{complaint.complaint_type}</ListItemText>
-            </ListItem>
-          )
-        })}
-        <LineGraph data={data} />
+          <Paper>
+            <Tabs
+              value={0}
+              onChange={() => console.log('hi')}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+            >
+              <Tab label="All Complaints" />
+              <Tab label="Verified 311 Complaints" />
+              <Tab label="User Complaints" />
+            </Tabs>
+          </Paper>
+        </Container>
+        <br />
       </div>
     )
   }
 }
 
-export default ExampleComplaints
+InfoPage.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(InfoPage)
