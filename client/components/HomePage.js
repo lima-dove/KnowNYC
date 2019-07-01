887dc1b51ee218b04eb396eedea56e711be76301
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2019-07-01 13:11:34 -0400

    fix: axes now render immediately on popup after marker click (add lifecycle event to render axes on component mount)

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -68,6 +68,7 @@ export default class HomePage extends Component {
     const locationComplaints = complaints.filter(
       complaint => complaint.location
     )
+    console.log('render')
 
     return (
       <div>
