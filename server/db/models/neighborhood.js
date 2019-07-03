7391b5caf2baf358517124daec222451d72ae941
Noah Schefer <nbschefer@gmail.com>
2019-07-03 15:59:23 -0400

    feat: add center coords to hood model

--- server/db/models/neighborhood.js
+++ server/db/models/neighborhood.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Neighborhood = db.define('neighborhood', {
-  name: Sequelize.STRING
+  name: Sequelize.STRING,
+  center_latitude: Sequelize.FLOAT,
+  center_longitude: Sequelize.FLOAT
 })
 
 module.exports = Neighborhood
