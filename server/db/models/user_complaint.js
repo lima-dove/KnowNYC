7469d38e8fe68f5dd4919a4602cbb14ea876823b
Noah Schefer <nbschefer@gmail.com>
2019-07-08 17:39:34 -0400

    feat: add user_complaint table

---
+++ server/db/models/user_complaint.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const UserComplaint = db.define('user_complaint', {
+  incident_address: Sequelize.STRING,
+  complaint_type: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  descriptor: {
+    type: Sequelize.TEXT,
+    validate: {
+      notEmpty: true
+    }
+  },
+  resolved: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  resolution_description: {
+    type: Sequelize.TEXT
+  }
+})
+
+module.exports = UserComplaint
