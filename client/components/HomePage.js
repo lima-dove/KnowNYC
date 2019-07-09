35790abeb566fe4c64a8462dc5e72a14391ac570
Svetlana Shinkar <svetanek@gmail.com>
2019-07-09 16:49:24 -0400

    feat: show borders of neighborhoods

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -3,7 +3,15 @@ import Button from '@material-ui/core/Button'
 import {withStyles} from '@material-ui/core/styles'
 import axios from 'axios'
 import React, {Component} from 'react'
-import MapGL, {FlyToInterpolator, Marker, Popup} from 'react-map-gl'
+import {render} from 'react-dom'
+import MapGL, {
+  FlyToInterpolator,
+  LinearInterpolator,
+  Marker,
+  Popup
+} from 'react-map-gl'
+import WebMercatorViewport from 'viewport-mercator-project'
+import bbox from '@turf/bbox'
 import greenDot from '../../markers/green-circle.png'
 import greenPointer from '../../markers/green-marker.png'
 import redDot from '../../markers/red-circle.png'
@@ -12,6 +20,7 @@ import InfoPage from './InfoPage'
 import NeighborhoodInfoPage from './NeighborhoodInfoPage'
 import SearchBar from './SearchBar'
 import Sidebar from './Sidebar'
+import MAP_STYLE from '../../public/MapStyle'
 
 const styles = theme => ({
   button: {
@@ -48,7 +57,7 @@ class HomePage extends Component {
       mouse: false
     }
     this.handleSearchClick = this.handleSearchClick.bind(this)
-    this.handleMapClick = this.handleMapClick.bind(this)
+    // this.handleMapClick.bind(this)
     this.handleNeighborhoodMarkerClick = this.handleNeighborhoodMarkerClick.bind(
       this
     )
@@ -58,6 +67,8 @@ class HomePage extends Component {
     this.mouseHandle = this.mouseHandle.bind(this)
     this.onCloseAddressPopup = this.onCloseAddressPopup.bind(this)
     this.mapRef = React.createRef()
+    this._map = React.createRef()
+    this._onClick = this._onClick.bind(this)
   }
 
   async componentDidMount() {
@@ -65,6 +76,39 @@ class HomePage extends Component {
     this.setState({
       neighborhoodComplaints: data
     })
+  }
+
+  _updateViewport = viewport => {
+    this.setState({viewport})
+  }
+
+  _onClick = event => {
+    const feature = event.features[1]
+    if (feature) {
+      // calculate the bounding box of the feature
+      const [minLng, minLat, maxLng, maxLat] = bbox(feature)
+      // construct a viewport instance from the current state
+      const viewport = new WebMercatorViewport(this.state.viewport)
+      const {longitude, latitude, zoom} = viewport.fitBounds(
+        [[minLng, minLat], [maxLng, maxLat]],
+        {
+          padding: 40
+        }
+      )
+
+      this.setState({
+        viewport: {
+          ...this.state.viewport,
+          longitude,
+          latitude,
+          zoom,
+          transitionInterpolator: new LinearInterpolator({
+            around: [event.offsetCenter.x, event.offsetCenter.y]
+          }),
+          transitionDuration: 1000
+        }
+      })
+    }
   }
 
   async handleSearchClick() {
@@ -148,9 +192,9 @@ class HomePage extends Component {
     })
   }
 
-  handleMapClick = e => {
-    e.preventDefault()
-  }
+  // handleMapClick = e => {
+  //   e.preventDefault()
+  // }
 
   handleSeeMoreClick = complaint => {
     //Redirect to Info Page Logic
@@ -238,17 +282,22 @@ class HomePage extends Component {
       <div>
         <MapGL
           id="mapGl"
+          ref={this._map}
           scrollZoom={scrollZoom}
           {...viewport}
           width="100vw"
           height="88vh"
-          minZoom={11}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          onViewportChange={v => this.handleViewChange(v)}
+          // minZoom={11}
+          mapStyle={MAP_STYLE}
+          interactiveLayerIds={['nyc-neighborhoods-fill']}
+          // mapStyle="mapbox://styles/mapbox/streets-v9"
+          onClick={this._onClick}
+          onViewportChange={this._updateViewport}
+          // onViewportChange={v => this.handleViewChange(v)}
           preventStyleDiffing={false}
           ref={map => (this.mapRef = map)}
           mapboxApiAccessToken={token}
-          onClick={this.handleMapClick}
+          // onClick={this.handleMapClick}
           transitionDuration={selectedAddress ? 2000 : 0}
           transitionInterpolator={
             selectedAddress ? new FlyToInterpolator() : null
@@ -379,6 +428,10 @@ class HomePage extends Component {
       </div>
     )
   }
+}
+
+export function renderToDom(container) {
+  render(<HomePage />, container)
 }
 
 export default withStyles(styles)(HomePage)
