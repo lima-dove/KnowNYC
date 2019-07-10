fe9e425b0bf2ede9891a3dfc6def01b36cb2d99d
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2019-07-10 16:06:04 -0400

    refactor: get rid of map actions from store

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,8 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
-import map from './map'
 
-const reducer = combineReducers({user, map})
+const reducer = combineReducers({user})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -13,4 +12,3 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
-export * from './map'
